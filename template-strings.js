7820da1fcf77e94cd4dfc71419c1b640a53f1565
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2026-04-28 15:24:38 +0000

    template

--- template-strings.js
+++ template-strings.js
@@ -1,4 +1,8 @@
-const nome = "Professor Jairo"
-const apresentacao = "Meu nome é " + nome
+const nome = "Professor Jairo";
+const idade = 2026-1986;
+const cidadeDeNascimento = "Campo Mourão";
 
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + "e nasci na cidade de" + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade ${cidadeDeNascimento}`;
 console.log(apresentacao)
